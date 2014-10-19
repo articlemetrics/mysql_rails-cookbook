b793a4ecfebd84192bd8ec00d51d05a487360dc4
Martin Fenner <mfenner@plos.org>
2014-10-19 23:18:33 +0200

    don't create database.yml file

--- providers/default.rb
+++ providers/default.rb
@@ -8,44 +8,7 @@ def load_current_resource
   @current_resource = Chef::Resource::MysqlRails.new(new_resource.name)
 end
 
-action :config do
-  # create user
-  user new_resource.user do
-    system true
-    gid new_resource.group
-    shell "/bin/bash"
-  end
-
-  # create required folders and set permissions
-  %W{ #{new_resource.name} #{new_resource.name}/current #{new_resource.name}/current/config #{new_resource.name}/shared #{new_resource.name}/shared/config }.each do |dir|
-    directory "/var/www/#{dir}" do
-      owner new_resource.user
-      group new_resource.group
-      mode '0755'
-      recursive true
-    end
-  end
-
-  # create database settings file
-  %w{ current shared }.each do |dir|
-    template "/var/www/#{new_resource.name}/#{dir}/config/database.yml" do
-      source 'database.yml.erb'
-      owner new_resource.user
-      group new_resource.group
-      mode '0644'
-      cookbook 'mysql_rails'
-      variables(
-        :application => new_resource.name,
-        :username    => new_resource.username,
-        :password    => new_resource.password,
-        :host        => new_resource.host
-      )
-      action :create
-    end
-  end
-end
-
-action :setup do
+action :create do
   run_context.include_recipe 'ruby::empty'
   run_context.include_recipe 'mysql::server'
   run_context.include_recipe 'database::mysql'
@@ -55,7 +18,7 @@ action :setup do
     action :create
   end
 
-  # create database user with all privileges for the table we just created
+  # create database user with all privileges
   mysql_database_user new_resource.username do
     connection mysql_connection_info
     password   new_resource.password
@@ -74,13 +37,6 @@ action :drop do
   mysql_database "#{new_resource.name}_#{new_resource.rails_env}" do
     connection mysql_connection_info
     action :drop
-  end
-end
-
-action :cleanup do
-  file "/var/www/#{new_resource.name}/shared/config/database.yml" do
-    action :delete
-    new_resource.updated_by_last_action(true)
   end
 end
 
--- resources/default.rb
+++ resources/default.rb
@@ -1,10 +1,8 @@
-actions :config, :setup, :migrate, :drop, :cleanup
-default_action :config
+actions :create, :drop
+default_action :create
 
 attribute :name, :kind_of => String, :name_attribute => true
 attribute :rails_env, :kind_of => String, :default => node['ruby']['rails_env']
-attribute :user, :kind_of => String, :default => node['ruby']['user']
-attribute :group, :kind_of => String, :default => node['ruby']['group']
 attribute :username, :kind_of => String, :default => node['ruby']['db']['username']
 attribute :password, :kind_of => String, :default => node['ruby']['db']['password']
 attribute :host, :kind_of => String, :default => node['ruby']['db']['host']
