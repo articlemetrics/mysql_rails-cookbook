c29fecc1333408001b07393a4760fb252fcf8f71
Martin Fenner <mfenner@plos.org>
2014-10-20 11:15:27 +0200

    use ENV variables

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'mfenner@plos.org'
 license          'Apache 2.0'
 description      'Installs and configures MySQL for Rails applications'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "0.3.0"
+version           "0.3.1"
 depends           "mysql"
 depends           "database"
 depends           "ruby", "~> 0.3.0"
