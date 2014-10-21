d8e55bf08412b2c2eb32e245dc31844e81f983d8
Martin Fenner <mfenner@plos.org>
2014-10-21 14:59:22 +0200

    use consistent default root password

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,5 +4,5 @@ default['mysql']['server_root_password'] = SecureRandom.hex(10)
 
 default['ruby']['rails_env'] = "development"
 default['mysql']['username'] = "root"
-default['mysql']['password'] = nil
+default['mysql']['password'] = node['mysql']['server_root_password']
 default['mysql']['host'] = "localhost"
