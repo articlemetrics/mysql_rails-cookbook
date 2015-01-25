7d27f195b7fce3bb2613a6ce6f75d0bc70c0c625
Martin Fenner <mfenner@plos.org>
2015-01-25 18:59:54 +0100

    don't include internal dependency mysql_chef-gem

--- metadata.rb
+++ metadata.rb
@@ -4,8 +4,7 @@ maintainer_email 'mfenner@plos.org'
 license          'Apache 2.0'
 description      'Installs and configures MySQL for Rails applications'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "0.3.13"
+version           "0.3.14"
 depends           "mysql"
-depends           "mysql-chef_gem", "~> 1.0.0"
 depends           "database"
 depends           "ruby", "~> 0.6.0"
