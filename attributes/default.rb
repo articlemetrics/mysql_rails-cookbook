a1ccd25ddc9566a13f56b540d011aef8d888b1d7
Martin Fenner <mfenner@plos.org>
2015-01-25 21:59:02 +0100

    install mysql 5.5

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,3 +3,4 @@ default['ruby']['rails_env'] = "development"
 default['mysql']['username'] = "root"
 default['mysql']['password'] = node['mysql']['initial_root_password']
 default['mysql']['host'] = "127.0.0.1"
+default['mysql']['version'] = "5.5"
