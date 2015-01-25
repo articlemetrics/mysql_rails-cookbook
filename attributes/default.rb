5c37e68a18dbe7f69d644b1ba46c29b8fdffea7d
Martin Fenner <mfenner@plos.org>
2015-01-25 21:42:20 +0100

    use 127.0.0.1 instead of localhost

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,5 +2,5 @@ default['ruby']['rails_env'] = "development"
 
 default['mysql']['username'] = "root"
 default['mysql']['password'] = node['mysql']['initial_root_password']
-default['mysql']['host'] = "localhost"
+default['mysql']['host'] = "127.0.0.1"
 default['mysql']['version'] = "5.6"
