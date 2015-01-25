4eb7acc6bfd855c12f210d7f52b27bf8b8ca52ee
Martin Fenner <mfenner@plos.org>
2015-01-25 18:43:30 +0100

    require mysql-chef_gem cookbook

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,8 @@ maintainer_email 'mfenner@plos.org'
 license          'Apache 2.0'
 description      'Installs and configures MySQL for Rails applications'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "0.3.12"
+version           "0.3.13"
 depends           "mysql"
+depends           "mysql-chef_gem", "~> 1.0.0"
 depends           "database"
 depends           "ruby", "~> 0.6.0"
