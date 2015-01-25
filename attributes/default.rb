9feea9bdfcb8d2c3d9d3f48d489b47e6accbd7b0
Martin Fenner <mfenner@plos.org>
2015-01-25 20:50:03 +0100

    define default mysql version

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,3 +3,4 @@ default['ruby']['rails_env'] = "development"
 default['mysql']['username'] = "root"
 default['mysql']['password'] = node['mysql']['initial_root_password']
 default['mysql']['host'] = "localhost"
+default['mysql']['version'] = "5.6"
