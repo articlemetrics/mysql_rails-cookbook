65457bffc066403da6bd824da7643cc3729a09c7
Martin Fenner <mfenner@plos.org>
2015-01-25 17:46:20 +0100

    removed obsolete parameter

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'mfenner@plos.org'
 license          'Apache 2.0'
 description      'Installs and configures MySQL for Rails applications'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "0.3.9"
+version           "0.3.11"
 depends           "mysql"
 depends           "database"
 depends           "ruby", "~> 0.6.0"
