8ab662cf7d3e5efd690264afe27ffb640c1f0274
Martin Fenner <mfenner@plos.org>
2015-01-26 01:26:10 +0100

    install mysql 5.6

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,5 +2,6 @@ default['ruby']['rails_env'] = "development"
 
 default['mysql']['username'] = "root"
 default['mysql']['password'] = node['mysql']['server_root_password']
-default['mysql']['host'] = "localhost"
+default['mysql']['host'] = "127.0.0.1"
+default['mysql']['server_package_version'] = "5.6"
 default['mysql']['enable_utf8'] = true
