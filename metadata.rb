25ddc5987bf5aa10cc3aacfe8482cacf8ac26a64
Martin Fenner <mfenner@plos.org>
2015-07-02 23:50:39 -0700

    install nodejs via ruby cookbook

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'mfenner@plos.org'
 license          'Apache 2.0'
 description      'Installs and configures MySQL for Rails applications'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "0.3.30"
+version           "0.3.31"
 depends           "mysql", "~> 5.6.1"
 depends           "database", "~> 2.3.1"
-depends           "ruby", "~> 0.6.0"
+depends           "ruby", "~> 0.7.0"
