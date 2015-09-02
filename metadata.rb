75746eaa26e00330b4cb5e894ebf3eae20cbddc4
Martin Fenner <martin.fenner@datacite.org>
2015-09-02 14:03:14 +0200

    don't run cookbook if mysql is installed differently

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'mfenner@plos.org'
 license          'Apache 2.0'
 description      'Installs and configures MySQL for Rails applications'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "0.3.32"
+version           "0.3.33"
 depends           "mysql", "~> 5.6.1"
 depends           "database", "~> 2.3.1"
 depends           "ruby", "~> 0.7.0"
